01021144d727be46942d90df6e3df68146be3e74
Valeriy Kuzmin <malcoriel@gmail.com>
2022-04-18 14:29:13 +0200

    fix: remove js build results

--- intro/ClassA.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UsefulStuff = exports.A = void 0;
-const ClassB_1 = require("./ClassB");
-class A extends ClassB_1.B {
-}
-exports.A = A;
-exports.UsefulStuff = "Useful";
--- intro/ClassB.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.B = void 0;
-const ClassA_1 = require("./ClassA");
-console.log(ClassA_1.UsefulStuff);
-class B {
-}
-exports.B = B;
